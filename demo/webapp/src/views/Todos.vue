bc53983fb761e8fef38a962373649b2cecbce2c8
annnenpolka <k.miyakewiss1@gmail.com>
2020-07-12 14:31:48 +0900

    create todos page

---
+++ demo/webapp/src/views/Todos.vue
@@ -0,0 +1,28 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'Todo',
+  data() {
+    return {
+      todos: [],
+    };
+  },
+  created: async function() {
+    await this.refresh();
+  },
+  methods: {
+    refresh: async function() {
+      const res = await axios.get('http://localhost:8080/');
+      this.todos = res.data.todos;
+      console.info(this.todos);
+    },
+  },
+};
+</script>
+
+<style scoped></style>
