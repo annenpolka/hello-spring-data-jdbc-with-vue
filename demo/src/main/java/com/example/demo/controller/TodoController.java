fabf72a1fe57ab79f13a73290f29c6b502c1fea1
annnenpolka <k.miyakewiss1@gmail.com>
2020-07-12 12:42:21 +0900

    add todoResponse with ResponseEntity

--- demo/src/main/java/com/example/demo/controller/TodoController.java
+++ demo/src/main/java/com/example/demo/controller/TodoController.java
@@ -2,7 +2,10 @@ package com.example.demo.controller;
 
 import com.example.demo.domain.Todo;
 import com.example.demo.repository.TodoRepository;
+import com.example.demo.response.TodoResponse;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,11 +16,13 @@ public class TodoController {
 
     TodoController(TodoRepository todoRepository) {
         this.todoRepository = todoRepository;
-   }
+    }
 
     @GetMapping("/")
-    public Iterable<Todo> getAll() {
-        return todoRepository.findAll();
+    public ResponseEntity<TodoResponse> getAll() {
+        Iterable<Todo> todos = todoRepository.findAll();
+        TodoResponse todoResponse = TodoResponse.builder().todos(todos).build();
+        return new ResponseEntity<>(todoResponse, HttpStatus.OK);
     }
 
 }
