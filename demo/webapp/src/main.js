17fcfd97ef1c485b8a0515f11cfcfbeb67d7a7aa
annnenpolka <k.miyakewiss1@gmail.com>
2020-07-21 14:44:10 +0900

    add ElementUI

--- demo/webapp/src/main.js
+++ demo/webapp/src/main.js
@@ -5,6 +5,12 @@ import store from './store'
 
 Vue.config.productionTip = false
 
+import ElementUI from 'element-ui'
+import locale from 'element-ui/lib/locale/lang/ja'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(ElementUI, {locale})
+
 new Vue({
   router,
   store,
