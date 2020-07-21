535337613995c6b77dcafd1637e970a18bed39ef
annnenpolka <k.miyakewiss1@gmail.com>
2020-07-21 14:46:03 +0900

    fix import order

--- demo/webapp/src/main.js
+++ demo/webapp/src/main.js
@@ -1,18 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import ElementUI from 'element-ui';
+import locale from 'element-ui/lib/locale/lang/ja';
+import 'element-ui/lib/theme-chalk/index.css';
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-import ElementUI from 'element-ui'
-import locale from 'element-ui/lib/locale/lang/ja'
-import 'element-ui/lib/theme-chalk/index.css'
-
-Vue.use(ElementUI, {locale})
+Vue.use(ElementUI, { locale });
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount('#app');
